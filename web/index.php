5962b72e817030e4bd70aaa0ca0b29152d8ea3a8
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-01-13 13:58:41 -0800

    started working on week2 team activity

--- web/index.php
+++ web/index.php
@@ -2,4 +2,19 @@
 
   phpinfo();
 
-?>
+?><!DOCTYPE html>
+<html>
+
+<head>
+    <title>CSE 341 | Ellen Carlson</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <ul>
+      <li><a href="week01/hello.html">Week 01</a></li>
+      <li><a href="week02/teamactivity.html">Week 02</a></li>
+    </ul>
+</body>
+
+</html>
