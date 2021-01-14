00e45b959c0583bf9fd55a6b4a5669eb1154ff4d
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-01-13 17:15:36 -0800

    forgot navbar and body html changes

--- web/index.php
+++ web/index.php
@@ -8,13 +8,34 @@
   <link rel="stylesheet" href="sass/style.css">
 </head>
 
+<nav>
+  <ul class="navbar">
+    <li><a href="#">Home</a></li>
+    <li class="dropdown"><a href="#">Assignments</a>
+      <div class="dropdown-menu">
+        <a href="#">Week 01</a>
+        <a href="#">Week 02</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+
 <body>
-  <div>
-    <img src="images/Ellen.jpg" alt="Portrait of Ellen">
-    <div>
+<div class="container">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCGEvDyTBaWhglasxEnXefMl0-tS3QlzOQow&usqp=CAU" alt="Portrait of Ellen">
+  <div class="text-holder">
+    <span>
       <h1>Ellen Carlson</h1>
-      <h2>Applied Technology Student</h2>
-      <p>Some filler text to fill this paragraph space.</p>
+      <h4>Applied Technology Student</h4>
+    </span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a lorem lorem. Nunc sed nunc fringilla, tincidunt justo eu, efficitur justo. Praesent non metus tellus. Nulla sed fermentum tellus. Proin varius quam vitae maximus fermentum.</p>
+    <ul>
+      <li><i class="fas fa-phone-alt"></i> 123-456-7890</li>
+      <li><i class="fas fa-envelope"></i> fake@email.com</li>
+    </ul>
+    <div>
+      <i class="fab fa-linkedin"></i>
+    </div>
     </div>
   </div>
 </body>
