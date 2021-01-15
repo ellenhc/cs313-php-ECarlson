376ecea6232976e3fda7cc295f5c66825438d109
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-01-15 00:39:42 -0800

    continued working on homepage

--- web/assignments.php
+++ web/assignments.php
@@ -1,4 +1,5 @@
-<?php?><!DOCTYPE html>
+<?php?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,12 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CSE 341 | Ellen Carlson</title>
   <link rel="stylesheet" href="sass/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 </head>
 
 <nav>
   <ul class="navbar">
     <li><a href="index.php">Home</a></li>
-    <li class="dropdown"><a href="assignments.php">Assignments</a>
+    <li class="dropdown"><a href="assignments.php">Assignments <i class="fas fa-angle-down"></i></a>
       <div class="dropdown-menu">
         <a href="week01/hello.html">Week 01</a>
         <a href="week02/teamactivity.html">Week 02</a>
@@ -21,18 +23,53 @@
 </nav>
 
 <body>
-  <div class="assignment-container">
-  <h1>Ellen Carlson's CSE 341 Assignments</h1>
-    <ul>
-      <li><a href="week01/hello.html">Week 01</a></li>
-      <li>Week 02
-        <ul>
-          <li><a href="week02/teamactivity.html">-02 Teach: Team Activity</a></li>
-          <li><a>-02 Prove: Assignment - Homepage</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
+<div class="solid-background"></div>
+  <main>
+    <h1>Ellen Carlson's CSE 341 Assignments</h1>
+    <div class="assignment-container main-card">
+      <div class="assignment-card">
+        <h5>Week 01</h5>
+        <h6>Hello World</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="week01/hello.html">Prove Assignment</a>
+      </div>
+      <div class="assignment-card">
+        <h5>Week 02</h5>
+        <h6>Card subtitle</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="week02/teamactivity.html">Team Activity</a>
+        <a href="index.php">Prove Assignment</a>
+      </div>
+      <div class="assignment-card">
+        <h5>Week 03</h5>
+        <h6>Card subtitle</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#">Team Activity</a>
+        <a href="#">Prove Assignment</a>
+      </div>
+      <div class="assignment-card">
+        <h5>Week 04</h5>
+        <h6>Card subtitle</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#">Team Activity</a>
+        <a href="#">Prove Assignment</a>
+      </div>
+      <div class="assignment-card">
+        <h5>Week 05</h5>
+        <h6>Card subtitle</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#">Team Activity</a>
+        <a href="#">Prove Assignment</a>
+      </div>
+      <div class="assignment-card">
+        <h5>Week 06</h5>
+        <h6>Card subtitle</h6>
+        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#">Team Activity</a>
+        <a href="#">Prove Assignment</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
-<?php?><!DOCTYPE html>
+<?php?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -12,7 +13,7 @@
 <nav>
   <ul class="navbar">
     <li><a href="index.php">Home</a></li>
-    <li class="dropdown"><a href="assignments.php">Assignments</a>
+    <li class="dropdown"><a href="assignments.php">Assignments <i class="fas fa-angle-down"></i></a>
       <div class="dropdown-menu">
         <a href="week01/hello.html">Week 01</a>
         <a href="week02/teamactivity.html">Week 02</a>
@@ -22,24 +23,36 @@
 </nav>
 
 <body>
-<div class="about-container">
-  <img src="images/Ellen1.jpg" alt="Portrait of Ellen">
-  <div class="text-holder">
-    <span>
-      <p>Greetings fellow aliens,</p>
-      <h1>Ellen Carlson</h1>
-      <h2>Applied Technology Student</h2>
-    </span>
-    <p>I'm a Seattle-based web development student studying online at BYU-Idaho. I've earned a certificate in web frontend and am finishing up work for web backend and graphic design. I have experience using HTML5, CSS, JavaScript, PHP, C++, C#, and Java.</p>
-    <ul>
-      <li><i class="fas fa-phone-alt"></i> 123-456-7890</li>
-      <li><i class="fas fa-envelope"></i> fake@email.com</li>
-    </ul>
-    <div class="social">
-      <a href="https://www.linkedin.com/in/ellenhowarth/" target="_blank"><i class="fab fa-linkedin"></i></a>
+  <div class="solid-background"></div>
+  <main>
+    <div class="about-container main-card">
+      <img src="images/Ellen-small.jpg" alt="Portrait of Ellen">
+      <div class="text-holder">
+        <span>
+          <p>Hello, I'm</p>
+          <h1>Ellen Carlson</h1>
+          <h3>Applied Technology Student</h3>
+        </span>
+        <p>I'm a Seattle-based web development student studying online at BYU-Idaho. I've earned a certificate in web frontend and am finishing up work for web backend and graphic design.</p>
+        <div>I have experience using C++, C#
+          <ul>
+            <li><i class="fab fa-html5"></i>HTML5</li>
+            <li><i class="fab fa-css3-alt"></i>CSS</li>
+            <li><i class="fab fa-php"></i>PHP</li>
+            <li><i class="fab fa-js-square"></i>JavaScript</li>
+            <li><i class="fab fa-java"></i>Java</li>
+            <li><i class="fas fa-code"></i>C++  C#</li>
+          </ul>
+        </div>
+        <div class="social">
+          <a href="https://www.linkedin.com/in/ellenhowarth/" target="_blank"><i class="fab fa-linkedin"></i></a>
+          <a href="https://byui-cse341-21w-02.slack.com" target="_blank"><i class="fab fa-slack"></i></a>
+          <a href="https://github.com/ellenhc/cs313-php-ECarlson" target="_blank"><i class="fab fa-github-square"></i></a>
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
+  </main>
+  <footer></footer>
 </body>
 
 </html>
