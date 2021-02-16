3c37914198250fd0e2043a5b97029e66c9d93362
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-15 23:29:21 -0800

    working on client side password check

--- web/week07/view/register.php
+++ web/week07/view/register.php
@@ -6,24 +6,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script>
+        $(document).ready(function() {
+                    $("#submitButton").click(function(e) {
+                        e.preventDefault();
+                        var matched,
+                            password = $("#userPassword").val(),
+                            confirm = $("#confirmPassword").val();
+
+                        matched = (password == confirm) ? true : false;
+                        if (matched) {
+                            //Submit line commented out for example.  In production, remove the //
+                            $("#register-form").submit(); 
+
+                            //Shows success message and prevents submission.  In production, comment out the next 2 lines.
+                            $("#responseDiv").html("Passwords Match");
+                            return false;
+                        } else {
+                            $("#responseDiv").html("Passwords don't match...");
+                            return false;
+                        }
+                    });
+    </script>
 </head>
 
 <body>
     <h1>Create an account</h1>
-    <?php 
+    <?php
     if (isset($message)) {
         echo "<b>$message</b>";
     }
     ?>
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" id="register-form">
         <label for="username">Name</label>
         <input type="text" id="username" name="username" required>
         <p class='info'>Password must be at least 7 characters and contain a number.</p>
-        <label for="userPassword">Password <?php if(isset($_SESSION['noMatch'])){echo $_SESSION['noMatch'];}?></label>
+        <label for="userPassword">Password <?php if (isset($_SESSION['noMatch'])) {
+                                                echo $_SESSION['noMatch'];
+                                            } ?></label>
         <input type="password" id="userPassword" name="userPassword" pattern="(?=^.{7,}$)(?=.*\d)(?=.*[a-z]).*$" required>
-        <label for="confirmPassword">Confirm Password <?php if(isset($_SESSION['noMatch'])){echo $_SESSION['noMatch'];}?></label>
+        <label for="confirmPassword">Confirm Password <?php if (isset($_SESSION['noMatch'])) {
+                                                            echo $_SESSION['noMatch'];
+                                                        } ?></label>
         <input type="password" id="confirmPassword" name="confirmPassword" required>
-        <input type="submit" name="submit" value="Register">
+        <div id="responseDiv"></div>
+        <input type="submit" name="submit" value="Register" id="submitButton">
         <input type="hidden" name="action" value="Register">
         <p>Already have an account? <a href="index.php?action=login">Log in</a></p>
     </form>
